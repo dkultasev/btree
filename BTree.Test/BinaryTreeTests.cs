46715fc4f1aa57d454329edfa5d9ad2e59560563
Dima <dkultasev@mail.ru>
2017-02-01 15:28:10 +0200

    initial commit

---
+++ BTree.Test/BinaryTreeTests.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BTree.Test
+{
+    [TestClass]
+    public class BinaryTreeTests
+    {
+        [TestMethod]
+        public void TestWhenIntegersInsertedQtyIsRight()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(17);
+            b.Insert(1);
+            b.Insert(10);
+            b.Insert(5);
+            b.Insert(17);
+            b.Insert(9);
+            b.Insert(20);
+            
+            Assert.AreEqual(b.Count, 6);
+        }
+
+        [TestMethod]
+        public void TestWhenIntegerValuesIsRemovedQtyIsRight()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(17);
+            b.Insert(1);
+            b.Insert(10);
+            b.Insert(5);
+            b.Insert(17);
+            b.Insert(9);
+            b.Insert(20);
+
+            b.Remove(17);
+            b.Remove(20);
+
+            Assert.AreEqual(b.Count, 4);
+
+        }
+
+        [TestMethod]
+        public void TestThanGreaterValueIsInsertedToTheRight()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(1);
+            b.Insert(10);
+
+            Assert.AreEqual(b.Root.Right.Value, 10);
+        }
+
+        [TestMethod]
+        public void TestWhenMultipleValuesInsertedThenFirstElementIsInsertedItSetAsRoot()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(1);
+            b.Insert(20);
+
+            Assert.AreEqual(b.Root.Value, 1);
+        }
+
+        [TestMethod]
+        public void TestWhenMultipleValuesInsertedThenMinValueIsUpdated()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(10);
+            b.Insert(20);
+            b.Insert(1);
+            b.Insert(2);
+
+            Assert.AreEqual(b.MinValue, 1);
+        }
+
+        [TestMethod]
+        public void TestWhenMultipleValuesInsertedThenMaxValueIsUpdated()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(10);
+            b.Insert(20);
+            b.Insert(1);
+            b.Insert(2);
+
+            Assert.AreEqual(b.MaxValue, 20);
+        }
+
+        [TestMethod]
+        public void Print()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(6);
+            b.Insert(7);
+            b.Insert(9);
+            b.Insert(4);
+            b.Insert(5);
+
+            b.Insert(10);
+            b.Insert(3);
+
+            b.Remove(4);
+            b.PrintLevelOrder();
+        }
+
+        [TestMethod]
+        public void TestWhenNodeWithNoRightDeletedThenLeftNodeTakesPlace()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(6);
+            b.Insert(5);
+            b.Insert(7);
+            b.Insert(9);
+            b.Insert(4);
+            b.Insert(10);
+            b.Insert(3);
+
+            b.Remove(4);
+
+            Assert.AreEqual(b.Root.Left.Left.Value,3);
+        }
+
+        [TestMethod]
+        public void TestWhenMinimalRightIsOnThe2ndLevel()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(6);
+            b.Insert(5);
+            b.Insert(8);
+            b.Insert(7);
+            b.Insert(9);
+            b.Insert(4);
+            b.Insert(10);
+            b.Insert(3);
+
+            Assert.AreEqual(b.GetMinimumFromRight(b.Root), b.Root.Right.Left);
+        }
+
+
+        [TestMethod]
+        public void TestRemoveWhenMinimalIsRoot()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(6);
+            b.Insert(5);
+            b.Insert(4);
+            b.Insert(3);
+
+
+            Assert.AreEqual(b.GetMinimumFromRight(b.Root), b.Root.Left);
+        }
+
+        [TestMethod]
+        public void TestWhenMinimalSubNodeWithNoLeft()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(6);
+            b.Insert(5);
+            b.Insert(8);
+            b.Insert(7);
+            b.Insert(9);
+            b.Insert(4);
+            b.Insert(10);
+            b.Insert(3);
+
+            Assert.AreEqual(b.GetMinimumFromRight(b.Root.Right.Right), b.Root.Right.Right.Right);
+        }
+
+        [TestMethod]
+        public void TestWhenMinimalSubNodeWithNoRight()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(6);
+            b.Insert(5);
+            b.Insert(8);
+            b.Insert(7);
+            b.Insert(9);
+            b.Insert(4);
+            b.Insert(10);
+            b.Insert(3);
+
+            Assert.AreEqual(b.GetMinimumFromRight(b.Root.Left.Left), b.Root.Left.Left.Left);
+        }
+
+        [TestMethod]
+        public void TestWhenMinimalSubNodeWithRight()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(6);
+            b.Insert(8);
+            b.Insert(7);
+            b.Insert(9);
+            b.Insert(4);
+            b.Insert(5);
+            b.Insert(10);
+            b.Insert(3);
+
+            Assert.AreEqual(b.GetMinimumFromRight(b.Root.Left), b.Root.Left.Right);
+        }
+
+        [TestMethod]
+        public void TestWhenLeftNodeWithLRIsDeleted()
+        {
+            var b = new BinaryTree<int>();
+            b.Insert(6);
+            b.Insert(7);
+            b.Insert(9);
+            b.Insert(4);
+            b.Insert(5);
+
+            b.Insert(10);
+            b.Insert(3);
+
+            b.Remove(4);
+
+            Assert.AreEqual(b.Root.Left.Left.Value, 3);
+        }
+    }
+}
+
+
---
+++ BTree/BinaryTree.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BTree
+{
+    public class Node<T>
+    {
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+        public T Value;
+    }
+    public class BinaryTree<T> where T : IComparable<T>
+    {
+
+        public Node<T> Root { get; set; }
+        public T MinValue { get; set; }
+        public T MaxValue { get; set; }
+        public int Count { get; private set; }
+
+        public void Insert(T value)
+        {
+            if (Count == 0)
+            {
+                Root = new Node<T>()
+                {
+                    Value = value
+                };
+                MinValue = MaxValue = value;
+                Count++;
+                return;
+            }
+
+            if (Root.Value.Equals(value)) return;
+            var tmp = Root;
+            while (true)
+            {
+                if (tmp.Value.CompareTo(value) > 0)
+                {
+                    if (tmp.Left == null)
+                    {
+                        tmp.Left = new Node<T>() { Value = value };
+                        Count++;
+                        break;
+                    }
+                    tmp = tmp.Left;
+                }
+                else if (tmp.Value.CompareTo(value) < 0)
+                {
+                    if (tmp.Right == null)
+                    {
+                        tmp.Right = new Node<T>() { Value = value };
+                        Count++;
+                        break;
+                    }
+                    tmp = tmp.Right;
+                }
+            }
+
+            SetMinValue(value);
+            SetMaxValue(value);
+        }
+
+
+
+        public void Remove(T value)
+        {
+            var current = Root;
+            var next = current;
+            Node<T> toDelete;
+
+            while (true)
+            {
+                current = next;
+
+                if (current.Value.CompareTo(value) > 0)
+                {
+                    if (current.Left.Value.Equals(value))
+                    {
+                        toDelete = GetMinimumFromRight(current.Left);
+                        Count--;
+                        if (toDelete == null) return;
+                        current.Left.Value = toDelete.Value;
+                        toDelete = null;
+                        return;
+                    };
+                    next = current.Left;
+                }
+                else if (current.Value.CompareTo(value) < 0)
+                {
+                    if (current.Right.Value.Equals(value))
+                    {
+                        toDelete = GetMinimumFromRight(current.Right);
+                        Count--;
+                        if (toDelete == null) return;
+                        current.Right.Value = toDelete.Value;
+                        toDelete = null;
+                        return;
+                    }
+                    ;
+                    next = current.Left;
+                }
+                else
+                {
+                    current = GetMinimumFromRight(current);
+                    Count--;
+                    return;
+                }
+            }
+        }
+
+        private void SetMinValue(T value)
+        {
+            if (value.CompareTo(MinValue) < 0)
+            {
+                MinValue = value;
+            }
+        }
+        private void SetMaxValue(T value)
+        {
+            if (value.CompareTo(MaxValue) > 0)
+            {
+                MaxValue = value;
+            }
+        }
+
+        public void PrintLevelOrder()
+        {
+            int h = Height(Root);
+            int i;
+            for (i = 1; i <= h; i++)
+            {
+                PrintGivenLevel(Root, i, "");
+                Console.WriteLine();
+            }
+        }
+
+        private int Height(Node<T> root)
+        {
+            if (root == null)
+                return 0;
+            else
+            {
+                int lheight = Height(root.Left);
+                int rheight = Height(root.Right);
+
+                if (lheight > rheight)
+                    return (lheight + 1);
+                else return (rheight + 1);
+            }
+        }
+
+        private void PrintGivenLevel(Node<T> root, int level, string position)
+        {
+            if (root == null)
+                return;
+            if (level == 1)
+                Console.Write(position + root.Value + "          ");
+            else if (level > 1)
+            {
+                PrintGivenLevel(root.Left, level - 1, "L(" + root.Value + ") - ");
+                PrintGivenLevel(root.Right, level - 1, "R(" + root.Value + ") - ");
+            }
+        }
+
+        public Node<T> GetMinimumFromRight<T>(Node<T> node)
+        {
+            if (node.Left == null && node.Right == null) return null;
+            if (node.Right == null) return node.Left;
+            if (node.Right.Left == null) return node.Right;
+            return node.Right.Left;
+        }
+    }
+}
